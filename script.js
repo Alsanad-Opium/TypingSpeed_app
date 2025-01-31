ae8b151b0a4d849636ee430bb6dfe18e683c3dae
Alsanad-Opium <alsanad112006@gmail.com>
2025-01-31 23:31:57 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+const id = document.getElementById('timer')
+
+const randomQoute = document.getElementById('random-qoute')
+
+const user_input = document.getElementById('user-input')
+
+const start_btn = document.getElementById('start-btn')
+
+let wpm = document.getElementById('wpm')
+
+let accuracy = document.getElementById('accuracy')
+
+let errors = document.getElementById('total-errors')
+
+const restart =  document.getElementById('restart-btn')
+

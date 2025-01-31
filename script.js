73ebeed11d6290544531cc927bed963d9e2a9f3f
Alsanad-Opium <alsanad112006@gmail.com>
2025-02-01 00:52:33 +0530

    added the style sheet link in  html and script was updated

--- script.js
+++ script.js
@@ -14,3 +14,34 @@ let errors = document.getElementById('total-errors')
 
 const restart =  document.getElementById('restart-btn')
 
+
+
+start_btn.addEventListener('click', randomQouteGenerator);
+
+async function randomQouteGenerator() {
+    try {
+        let response = await fetch('https://qapi.vercel.app/api/random');
+        let data = await response.json();
+
+        randomQoute.textContent = data.quote;
+        
+        qouteArray = new Array(data.quote.split(""))
+
+        user_input.addEventListener('input', check_errors)
+    
+        function check_errors() {
+            let user_value = new Array (user_input.value.split())
+        }
+    }
+     catch (error) {
+        console.error('Error fetching quote:', error);
+    }
+    
+}
+
+
+
+
+
+
+
